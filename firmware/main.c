b0bd9e68f18e73c6ee18cce02d583f0c83d5d2ae
shimniok@gmail.com <shimniok@gmail.com>
2017-04-29 14:25:46 -0600

    modified to light up pins in correct order

--- firmware/main.c
+++ firmware/main.c
@@ -8,9 +8,17 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#define DELAY 10
+#define PINS 17
+
+enum ports { PA, PB, PD };
+uint8_t pin[PINS]  = { PD0, PD1, PA1, PA0, PD2, PD3, PD4, PD5, PD6, PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7 };
+uint8_t port[PINS] = { PD,  PD,  PA,  PA,  PD,  PD,  PD,  PD,  PD,  PB,  PB,  PB,  PB,  PB,  PB,  PB,  PB };
+
+
 int main()
 {
-  char i;
+  uint8_t i;
 
   PORTA=0;
   PORTB=0;
@@ -20,24 +28,22 @@ int main()
   DDRD=0x7f;  // PD0-6
 
   while (1) {
-    for (i = 0 ; i < 2; i++) {
-      PORTA = (1 << i);
-      _delay_ms(300);
+    for (i = 0; i < PINS; i++) {
+      PORTA = 0;
+      PORTB = 0;
+      PORTD = 0;
+      switch (port[i]) {
+        case PA:
+          PORTA = (1<<pin[i]);
+          break;
+        case PB:
+          PORTB = (1<<pin[i]);
+          break;
+        case PD:
+          PORTD = (1<<pin[i]);
+          break;
+      }
+      _delay_ms(DELAY);
     }
-    PORTA = 0;
-
-    for (i = 0; i < 8; i++) {
-      PORTB = (1 << i);
-      _delay_ms(300);
-    }
-    PORTB = 0;
-
-    for (i = 0; i < 7; i++) {
-      PORTD = (1 << i);
-      _delay_ms(300);
-    }
-    PORTD = 0;
-
   }
-
 }
